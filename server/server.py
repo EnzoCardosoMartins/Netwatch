cbbc9a23b35a839509a69a4d6ad3045bfba54ae8
Enzo Cardoso Martins <enzocm.26@gmail.com>
2026-05-22 19:30:44 -0300

    Integracao com o banco de dados

--- server/server.py
+++ server/server.py
@@ -1,11 +1,15 @@
 import socket
 import threading
+import json
+from database import get_db_connection
+import time
 
 IP_LISTEN = "127.0.0.1"
 UDP_PORT = 9999
 TCP_PORT = 9998
 
 
+# Recebe os dados UDP (normais) do sensor
 def listen_udp():
     print(f"[*] Thread UDP ativa na porta {UDP_PORT}")
 
@@ -20,8 +24,33 @@ def listen_udp():
         mensagem = dados_bytes.decode('utf-8')
         print(f"\n[UDP] Recebido de {endereco_cliente}: {mensagem}")
 
+        try:
+            pacote = json.loads(mensagem)
+            sensor_id = pacote["sensor_id"]
+            payload = pacote["payload"]
+
+            conn = get_db_connection()
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                INSERT INTO metrics (sensor_id, cpu_percent, ram_percent, timestamp)
+                VALUES (?, ?, ?, ?)
+                """,
+                (sensor_id, payload["cpu_percent"], payload["ram_percent"], pacote["timestamp"])
+            )
+
+            conn.commit()
+            conn.close()
+            print(f"[UDP] Métricas do sensor {sensor_id} salvas no banco com sucesso!")
+            
+        except Exception as e:
+            print(f"[ERRO UDP] Falha ao processar ou salvar o pacote: {e}")
+
     pass
 
+
+# Recebe os dados TCP (Alertas) do sensor
 def listen_tcp():
     print(f"[*] Thread TCP ativa na porta {TCP_PORT}")
 
@@ -29,11 +58,48 @@ def listen_tcp():
     tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     tcp_socket.bind((IP_LISTEN, TCP_PORT))
     tcp_socket.listen(5)
+
     while True:
+        # Aceita a conexao
         conexao_cliente, endereco_cliente = tcp_socket.accept()
+        # Pega os bytes
         dados_bytes = conexao_cliente.recv(4096)
         mensagem = dados_bytes.decode('utf-8')
         print(f"\n[TCP] Recebido de {endereco_cliente}: {mensagem}")
+
+        try:
+            pacote = json.loads(mensagem)
+            sensor_id = pacote["sensor_id"]
+            payload = pacote["payload"]
+
+            conn = get_db_connection()
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                INSERT INTO events (sensor_id, type, category, severity, description, raw_payload, timestamp, received_at)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                """,
+                (
+                    sensor_id,
+                    pacote["type"],            # "ALERT"
+                    payload["category"],        # "FILE_INTEGRITY"
+                    payload["severity"],        # "CRITICAL"
+                    payload["description"],
+                    mensagem,                   # JSON completo para auditoria histórica
+                    pacote["timestamp"],
+                    time.time()                 # Momento exato em que o servidor recebeu
+                )
+            )
+
+            conn.commit()
+            conn.close()
+            print(f"[TCP 💾] Alerta crítico do sensor {sensor_id} salvo no banco com sucesso!")
+
+        except Exception as e:
+            print(f"[ERRO TCP] Falha ao processar ou salvar o alerta: {e}")
+
+        # Fecha o socket dedicado após terminar de tratar o cliente
         conexao_cliente.close()
 
 
